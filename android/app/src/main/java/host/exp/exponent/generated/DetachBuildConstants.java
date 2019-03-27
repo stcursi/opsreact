4b17f12e9e198aec96c8a7b8181e15d561ba5eec
Stefano Cursi <stcursi@gmail.com>
2019-03-27 22:39:17 +0100

    mappa base con presa di location automatica

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp7d809f9cd5e5488f93a7cd7d5bf9b9d9://af-6mb.stcursi.opsreact.exp.direct:80";
 
 }
